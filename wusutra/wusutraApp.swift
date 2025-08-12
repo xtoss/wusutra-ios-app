d26790a1f81ea994f045c13a542ce7bdf59b0b79
jamie <jamie.xue@gmail.com>
2025-08-12 12:08:16 +0800

    Initial Commit

---
+++ wusutra/wusutraApp.swift
@@ -0,0 +1,17 @@
+//
+//  wusutraApp.swift
+//  wusutra
+//
+//  Created by jxu on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct wusutraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
